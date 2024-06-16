befb7cdba22dba34ad850391be63398f302d3f24
je_martini <jesusmartini2017@gmail.com>
2024-06-16 13:59:06 -0400

    deploy3

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const options = {
     if(whitelist.includes(origin) || !origin){
       callback(null,true)
     } else {
-      callback(new Error('no permitido'))
+      callback(new Error('no permitido en vercel'))
     }
   }
 }
